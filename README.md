301d39c36b4de7ad20023be992fb8cd36215c077
anddyoleinikov <dyt321@ya.ru>
2018-04-13 10:54:48 +0300

    Made a script to find duplicates

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+With this script you can find if you have duplicates in specified folder and subfolders.
+You will recieve set of duplicates names and sizes.
+
+# Quickstart
+
+Specify the path to the directory when using this script from the command line.
+
+```bash
+$ python duplicates.py <path to a directory>
+You are looking for duplicates in <path to a directory>
+You have <amount of> duplicates:
+<file name> <file size>
+```
+
+
+### Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python duplicates.py /home
+You are looking for duplicates in /home
+You have 7 duplicates:
+master 41
+3ca6dc5aeb211beeab8e7d3ae5858c9cec1690 163
+__init__.py 276
+axes_rgb.py 387
+Catamarca 1109
+RECORD 2812
+py26compat.py 481
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,34 @@
+import os
+import sys
+from os.path import join, getsize, exists
+
+
+def find_duplicates(filepath):
+    files_set = set()
+    duplicates = set()
+    for root, dirs, files in os.walk(filepath):
+        for name in files:
+            name_and_size = (name, getsize(join(root, name)))
+            if name_and_size in files_set:
+                duplicates.add(name_and_size)
+                continue
+            files_set.add(name_and_size)
+    return duplicates
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        sys.exit('No path specified')
+    filepath = ' '.join(sys.argv[1:])
+    print('You are looking for duplicates in', filepath)
+    duplicates = find_duplicates(filepath)
+
+    if not exists(filepath):
+        sys.exit('Filepath doesn\'t exist')
+
+    elif duplicates == set():
+        sys.exit('No duplicates')
+
+    print('You have {} duplicates:'.format(len(duplicates)))
+    for name, size in duplicates:
+        print(name, size)
