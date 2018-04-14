cd33aeb37edb1d8ae7076bb1ce2c231aa0cc6d90
anddyoleinikov <dyt321@ya.ru>
2018-04-14 12:02:01 +0300

    changed set to deafultdict, minor fixes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Anti-Duplicator
 
 With this script you can find if you have duplicates in specified folder and subfolders.
-You will recieve set of duplicates names and sizes.
+You will recieve files names, sizes and paths.
 
 # Quickstart
 
@@ -11,7 +11,10 @@ Specify the path to the directory when using this script from the command line.
 $ python duplicates.py <path to a directory>
 You are looking for duplicates in <path to a directory>
 You have <amount of> duplicates:
-<file name> <file size>
+File name: <file name>
+File size: <file size>
+Paths:
+<Path to file>
 ```
 
 
@@ -19,16 +22,20 @@ You have <amount of> duplicates:
 
 ```bash
 
-$ python duplicates.py /home
-You are looking for duplicates in /home
-You have 7 duplicates:
-master 41
-3ca6dc5aeb211beeab8e7d3ae5858c9cec1690 163
-__init__.py 276
-axes_rgb.py 387
-Catamarca 1109
-RECORD 2812
-py26compat.py 481
+$ python duplicates.py /test/
+You are looking for duplicates in /test/
+You have 2 duplicates:
+File name: __init__.py
+File size: 0
+Paths:
+/test/env/lib/python3.5/site-packages/pip/operations
+/test/env/lib/python3.5/site-packages/pkg_resources/_vendor
+File name: INSTALLER
+File size: 4
+Paths:
+/test/env/lib/python3.5/site-packages/setuptools-20.7.0.dist-info
+/test/env/lib/python3.5/site-packages/pkg_resources-0.0.0.dist-info
+/test/env/lib/python3.5/site-packages/pip-8.1.1.dist-info
 
 ```
 
--- duplicates.py
+++ duplicates.py
@@ -1,34 +1,44 @@
 import os
 import sys
 from os.path import join, getsize, exists
+from collections import defaultdict
 
 
-def find_duplicates(filepath):
-    files_set = set()
-    duplicates = set()
-    for root, dirs, files in os.walk(filepath):
-        for name in files:
+def find_duplicates(dir_path):
+    files_and_paths = defaultdict(list)
+    for root, dirs, file_names in os.walk(dir_path):
+        for name in file_names:
             name_and_size = (name, getsize(join(root, name)))
-            if name_and_size in files_set:
-                duplicates.add(name_and_size)
-                continue
-            files_set.add(name_and_size)
+            files_and_paths[name_and_size].append(root)
+    duplicates = {
+        file: paths for file, paths in files_and_paths.items()
+        if len(paths) > 1
+    }
     return duplicates
+
+
+def print_duplicates(duplicates):
+    print('You have {} duplicates:'.format(len(duplicates)))
+    for file, paths in duplicates.items():
+        name, size = file
+        print('File name:', name)
+        print('File size:', size)
+        print('Paths:')
+        for path in paths:
+            print(path)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit('No path specified')
-    filepath = ' '.join(sys.argv[1:])
-    print('You are looking for duplicates in', filepath)
-    duplicates = find_duplicates(filepath)
+    dir_path = sys.argv[1]
+    print('You are looking for duplicates in', dir_path)
+    if not exists(dir_path):
+        sys.exit("Directory path doesn't exist")
 
-    if not exists(filepath):
-        sys.exit('Filepath doesn\'t exist')
+    duplicates = find_duplicates(dir_path)
 
-    elif duplicates == set():
+    if not duplicates:
         sys.exit('No duplicates')
 
-    print('You have {} duplicates:'.format(len(duplicates)))
-    for name, size in duplicates:
-        print(name, size)
+    print_duplicates(duplicates)
