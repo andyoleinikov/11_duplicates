aff10a89a63a665a6e86a431b1837d6d417d5f94
anddyoleinikov <dyt321@ya.ru>
2018-04-17 01:33:15 +0300

    changed variable names, duplicates printing

--- duplicates.py
+++ duplicates.py
@@ -1,17 +1,19 @@
 import os
 import sys
-from os.path import join, getsize, exists
+from os.path import join, getsize, isdir
 from collections import defaultdict
 
 
 def find_duplicates(dir_path):
     files_and_paths = defaultdict(list)
     for root, dirs, file_names in os.walk(dir_path):
-        for name in file_names:
-            name_and_size = (name, getsize(join(root, name)))
-            files_and_paths[name_and_size].append(root)
+        for file_name in file_names:
+            file_size = getsize(join(root, file_name))
+            files_and_paths[(file_name, file_size)].append(root)
+
     duplicates = {
-        file: paths for file, paths in files_and_paths.items()
+        (file_name, file_size): paths for (file_name, file_size), paths
+        in files_and_paths.items()
         if len(paths) > 1
     }
     return duplicates
@@ -19,13 +21,11 @@ def find_duplicates(dir_path):
 
 def print_duplicates(duplicates):
     print('You have {} duplicates:'.format(len(duplicates)))
-    for file, paths in duplicates.items():
-        name, size = file
-        print('File name:', name)
-        print('File size:', size)
+    for (file_name, file_size), paths in duplicates.items():
+        print('File name:', file_name)
+        print('File size:', file_size)
         print('Paths:')
-        for path in paths:
-            print(path)
+        print('\n'.join(paths))
 
 
 if __name__ == '__main__':
@@ -33,12 +33,14 @@ if __name__ == '__main__':
         sys.exit('No path specified')
     dir_path = sys.argv[1]
     print('You are looking for duplicates in', dir_path)
-    if not exists(dir_path):
-        sys.exit("Directory path doesn't exist")
+
+    if not isdir(dir_path):
+        sys.exit('Specified path is not a directory')
 
     duplicates = find_duplicates(dir_path)
 
-    if not duplicates:
-        sys.exit('No duplicates')
+    if duplicates:
+        print_duplicates(duplicates)
+        exit()
 
-    print_duplicates(duplicates)
+    print('No duplicates')
